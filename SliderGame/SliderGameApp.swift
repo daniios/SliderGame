3f86d100be23e0adb2ac921b733c4577cb091b5d
Daniil Chupin <danielchupin@yandex.ru>
2023-07-30 18:12:47 +0700

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Даниил Чупин on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
